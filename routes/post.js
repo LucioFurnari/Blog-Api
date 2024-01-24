8913d2de8739f505e39bc7b8a6baa13717084612
LucioFurnari <luciofurnari99@gmail.com>
2024-01-24 13:35:20 -0300

    Add get post route

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { create_post, get_posts } = require('../controllers/post');
+const { create_post, get_posts, get_post } = require('../controllers/post');
 
 // Create post POST route
 router.post('/posts', create_post);
 
 // GET all posts
 router.get('/posts', get_posts);
+
+// GET post
+router.get('/posts/:id', get_post);
 
 // Export router
 module.exports = router;
