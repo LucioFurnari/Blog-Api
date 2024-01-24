839ed46baa5eaae412c2a1b787db26bf4ee01f9a
LucioFurnari <luciofurnari99@gmail.com>
2024-01-24 17:28:27 -0300

    Add delete and put route

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { create_post, get_posts, get_post } = require('../controllers/post');
+const { create_post, get_posts, get_post, delete_post, update_post } = require('../controllers/post');
 
 // Create post POST route
 router.post('/posts', create_post);
@@ -10,6 +10,12 @@ router.get('/posts', get_posts);
 
 // GET post
 router.get('/posts/:id', get_post);
+
+// DELETE post
+router.delete('/posts/:id', delete_post);
+
+// UPDATE post
+router.put('/posts/:id', update_post);
 
 // Export router
 module.exports = router;
