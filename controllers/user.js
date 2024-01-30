1de4dc2e0c2336bb0e097002664cc9a447a6fdfc
LucioFurnari <luciofurnari99@gmail.com>
2024-01-29 21:04:05 -0300

    Add create user controller with bcryptjs

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,18 @@
 const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
 
 exports.create_user = async (req, res) => {
-  const user = new User({
-    name: req.body.user_name,
-    email: req.body.user_email,
-    password: req.body.user_password
-  });
+  bcryptjs.hash(req.body.password, 10, async (err, hashedPassword) => {
+    if (err) throw err;
+    else {
+      const user = new User({
+        name: req.body.user_name,
+        email: req.body.user_email,
+        password: hashedPassword,
+      });
+
+      await user.save();
+      res.status(200).json(user);
+    }
+  })
 };
