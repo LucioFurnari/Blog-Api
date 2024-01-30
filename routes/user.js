dab8869af76e66cf5ed38a5c7147e755f317c882
LucioFurnari <luciofurnari99@gmail.com>
2024-01-29 21:03:19 -0300

    Create POST router for create user, and export router

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { create_user } = require('../controllers/user');
 
 // Create user POST route
-router.post('/users')
+router.post('/users', create_user);
+
+
+// Export router
+module.exports = router;
