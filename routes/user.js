d531b69215080b90b0d9d62e48095b71915ba1b9
LucioFurnari <luciofurnari99@gmail.com>
2024-02-02 16:49:53 -0300

    Add logout post route

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { create_user, user_login, authLocal, get_user_info } = require('../controllers/user');
+const { create_user, user_login, user_logout, authLocal, get_user_info } = require('../controllers/user');
 
 // Create user POST route
 router.post('/users', create_user);
 
 // Login user POST route
 router.post('/login', authLocal, user_login);
+
+// Logout user POST route
+router.post('/logout', user_logout);
 
 // Get user info TEST
 router.get('/user', get_user_info);
