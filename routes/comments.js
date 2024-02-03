cbfb12d7b95d6ed0b007b7687c9508c3a5cd6232
LucioFurnari <luciofurnari99@gmail.com>
2024-02-03 15:58:56 -0300

    Add post and get routes for create and get comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { create_comments } = require('../controllers/comments');
+const { create_comment, get_all_comments } = require('../controllers/comments');
 
 // Create comment POST route
-router.post('/posts/:id/comments', create_comments);
+router.post('/posts/:id/comments', create_comment);
+
+// See all comments for the post GET route
+router.get('/posts/:id/comments', get_all_comments);
+
+
+// Export router
+module.exports = router;
