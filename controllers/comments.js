393ebd257adb844b8adfe7715295c161ca027d2e
LucioFurnari <luciofurnari99@gmail.com>
2024-02-03 15:58:06 -0300

    Add controller to get all coments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,7 +1,7 @@
-const Comments = require('../models/comment');
+const Comment = require('../models/comment');
 
 exports.create_comment = async (req, res) => {
-  const comment = new Comments({
+  const comment = new Comment({
     author: req.body.author,
     text: req.body.text,
     timestamp: req.body.timestamp,
@@ -12,3 +12,9 @@ exports.create_comment = async (req, res) => {
 
   res.status(200).json(comment);
 };
+
+exports.get_all_comments = async (req, res) => {
+  const comments = await Comment.find({});
+
+  res.status(200).json(comments);
+}
