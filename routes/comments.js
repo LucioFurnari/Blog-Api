bbe185bc1b4ba3fe642daeeb8cd4a0ad63676500
LucioFurnari <luciofurnari99@gmail.com>
2024-02-15 19:27:31 -0300

    Change :id for :postid in each post route

--- routes/comments.js
+++ routes/comments.js
@@ -3,19 +3,19 @@ const router = express.Router();
 const { create_comment, get_all_comments, get_comment, delete_comment, update_comment } = require('../controllers/comments');
 
 // Create comment POST route
-router.post('/posts/:id/comments', create_comment);
+router.post('/posts/:postid/comments', create_comment);
 
 // See all comments for the post GET route
-router.get('/posts/:id/comments', get_all_comments);
+router.get('/posts/:postid/comments', get_all_comments);
 
 // Get one individual comment GET route
-router.get('/posts/:id/comments/:id_comment', get_comment);
+router.get('/posts/:postid/comments/:id_comment', get_comment);
 
 // Delete comment DELETE route
-router.delete('/posts/:id/comments/:id_comment', delete_comment);
+router.delete('/posts/:postid/comments/:id_comment', delete_comment);
 
 // Update comment PUT route
-router.put('/posts/:id/comments/:id_comment', update_comment);
+router.put('/posts/:postid/comments/:id_comment', update_comment);
 
 // Export router
 module.exports = router;
