43fe1607f810a391bbc61d1c2088557c730c5cfd
LucioFurnari <luciofurnari99@gmail.com>
2024-02-14 23:50:34 -0300

    Remove passport authenticate

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { create_user, user_login, user_logout, authLocal, get_user_info } = require('../controllers/user');
+const { create_user, user_login, user_logout, get_user_info } = require('../controllers/user');
 
 // Create user POST route
 router.post('/users', create_user);
 
 // Login user POST route
-router.post('/login', authLocal, user_login);
+router.post('/login', user_login);
 
 // Logout user POST route
 router.post('/logout', user_logout);
