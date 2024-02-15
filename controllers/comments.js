4908b96fdcba7a22a48523aa0e90931972621cee
LucioFurnari <luciofurnari99@gmail.com>
2024-02-15 19:27:02 -0300

    Add validator to create comment controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,18 +1,33 @@
 const Comment = require('../models/comment');
+const { check, validationResult } = require('express-validator');
 
-exports.create_comment = async (req, res) => {
-  const comment = new Comment({
-    author: req.body.author,
-    text: req.body.text,
-    timestamp: req.body.timestamp,
-    response_to: req.body.response_to,
-    post: req.params.id,
-  });
+exports.create_comment =  [
+  check('author').trim().escape().notEmpty().withMessage('Author is required'),
+  check('text').trim().escape().notEmpty().withMessage('Text is required'),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-  await comment.save();
-
-  res.status(200).json(comment);
-};
+      if (!errors.isEmpty()) {
+        res.status(422).json({ errors: errors.array() });
+      } else {
+        const comment = new Comment({
+          author: req.user.username,
+          text: req.body.text,
+          timestamp: req.body.timestamp,
+          response_to: req.body.response_to,
+          post: req.params.postid,
+        });
+      
+        await comment.save();
+      
+        res.status(200).json(comment);
+      }
+    } catch (error) {
+      res.status(404).send(error);
+    }
+  }
+];
 
 exports.get_all_comments = async (req, res) => {
   const comments = await Comment.find({});
