65cfc9307f1568433bf7053c5130d8ac7a0bcdec
LucioFurnari <luciofurnari99@gmail.com>
2024-02-15 23:33:54 -0300

    Add validator to create post controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,17 +1,22 @@
 const Post = require('../models/post');
+const { check, validationResult } = require('express-validator');
 
-exports.create_post = async (req, res) => {
-  const post = Post({
-    title: req.body.title,
-    text: req.body.text,
-    author: req.body.author,
-    timestamp: req.body.timestamp,
-  });
-
-  await post.save();
-
-  res.status(200).json(post);
-};
+exports.create_post = [
+  check('title').trim().escape().notEmpty().withMessage('Title is required'),
+  check('text').trim().escape().notEmpty().withMessage('Text is required'),
+  async (req, res) => {
+    const post = Post({
+      title: req.body.title,
+      text: req.body.text,
+      author: req.body.author,
+      timestamp: req.body.timestamp,
+    });
+  
+    await post.save();
+  
+    res.status(200).json(post);
+  }
+]
 
 exports.update_post = async (req, res) => {
   const updatedPost = new Post({
