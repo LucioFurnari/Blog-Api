b66a1ae40aa3b62fc9dfaf5d47b73894c7cac4de
LucioFurnari <luciofurnari99@gmail.com>
2024-02-17 02:27:40 -0300

    Add validation request to create post

--- controllers/post.js
+++ controllers/post.js
@@ -4,33 +4,52 @@ const { check, validationResult } = require('express-validator');
 exports.create_post = [
   check('title').trim().escape().notEmpty().withMessage('Title is required'),
   check('text').trim().escape().notEmpty().withMessage('Text is required'),
+  check('author').trim().escape().notEmpty().withMessage('There is no author'),
+
   async (req, res) => {
-    const post = Post({
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        res.status(422).json({ errors: errors.array() });
+      } else {
+      const post = Post({
+        title: req.body.title,
+        text: req.body.text,
+        author: req.user.username,
+        timestamp: req.body.timestamp,
+      });
+
+      await post.save();
+
+      res.status(200).json(post);
+    }
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+]
+
+exports.update_post = [
+  check('title').trim().escape().notEmpty().withMessage('Title is required'),
+  check('text').trim().escape().notEmpty().withMessage('Text is required'),
+  
+  async (req, res) => {
+
+
+    const updatedPost = new Post({
       title: req.body.title,
       text: req.body.text,
       author: req.body.author,
       timestamp: req.body.timestamp,
+      _id: req.params.id,
     });
   
-    await post.save();
+    const post = await Post.findByIdAndUpdate(req.params.id, updatedPost, {});
   
     res.status(200).json(post);
-  }
+  },
 ]
-
-exports.update_post = async (req, res) => {
-  const updatedPost = new Post({
-    title: req.body.title,
-    text: req.body.text,
-    author: req.body.author,
-    timestamp: req.body.timestamp,
-    _id: req.params.id,
-  });
-
-  const post = await Post.findByIdAndUpdate(req.params.id, updatedPost, {});
-
-  res.status(200).json(post);
-};
 
 exports.get_posts = async (req, res) => {
   const posts = await Post.find({});
