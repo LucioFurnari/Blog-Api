be9050828d1c1bc7c74b5069a4919b44e9a8cfc0
LucioFurnari <luciofurnari99@gmail.com>
2024-02-18 20:55:37 -0300

    Remove logout route

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { create_user, user_login, user_logout, get_user_info } = require('../controllers/user');
+const { create_user, user_login, get_user_info } = require('../controllers/user');
 
 // Create user POST route
 router.post('/users', create_user);
 
 // Login user POST route
 router.post('/login', user_login);
-
-// Logout user POST route
-router.post('/logout', user_logout);
 
 // Get user info TEST
 router.get('/user', get_user_info);
