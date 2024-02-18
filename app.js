2bc91aee8488f0cd45d8b22fced0f4fecbb14080
LucioFurnari <luciofurnari99@gmail.com>
2024-02-18 20:54:48 -0300

    Remove passport js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const router = require('./routes/index');
-const passport = require('passport');
-const session = require('express-session');
 
 mongoose.connect(`mongodb+srv://luciofurnari99:${process.env.DB_PASSWORD}@blog-db.tgzgpnb.mongodb.net/Blog-DB?retryWrites=true&w=majority`)
 .catch((error) => console.log(error));
@@ -13,20 +11,9 @@ const app = express();
 // Configure cors
 app.use(cors());
 
-app.use(session({
-  secret: 'cat',
-  resave: false,
-  saveUninitialized: true,
-}));
-app.use(passport.authenticate('session'));
-app.use(passport.initialize());
 app.use(express.json());
 
 app.use('/api', router.post, router.user, router.comments);
-
-// app.use('/api', router.user);
-
-// app.use('/api', router.comments);
 
 app.listen(3000, () => {
   console.log('Server listen to port 3000!')
