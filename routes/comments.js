a3aba68aab6b7a091403d3b74ea83c8729442c95
LucioFurnari <luciofurnari99@gmail.com>
2024-04-20 23:49:19 -0300

    Add authenticate token to create comment

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { create_comment, get_all_comments, get_comment, delete_comment, update_comment } = require('../controllers/comments');
+const { authenticateToken } = require('../utils/auth');
 
 // Create comment POST route
-router.post('/posts/:postid/comments', create_comment);
+router.post('/posts/:postid/comments', authenticateToken, create_comment);
 
 // See all comments for the post GET route
 router.get('/posts/:postid/comments', get_all_comments);
